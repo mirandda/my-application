8181a34002d824e23d3c0c82942ecf9b31d76336
mirandda <mirande.morina@gmail.com>
2019-05-21 01:34:57 +0200

    Projekti-mCommerce

---
+++ app/src/main/java/com/projekti/mcommerce/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.projekti.mcommerce;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import io.paperdb.Paper;
+
+public class HomeActivity extends AppCompatActivity {
+private Button LogoutBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        LogoutBtn =(Button) findViewById(R.id.logout_btn);
+
+
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Paper.book().destroy();
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/projekti/mcommerce/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.projekti.mcommerce.Prevalent;
+
+import com.projekti.mcommerce.Model.Users;
+
+public class Prevalent {
+    private  static Users currentOnlineUser;
+
+    public static final String UserPhoneKey="UserPhone";
+    public  static final String UserPasswordKey="UserPassword";
+
+}
